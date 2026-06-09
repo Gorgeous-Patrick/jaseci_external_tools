241d42bbf78066c13aaac59102d1f72e719c0fa7
Patrick Li <baichuanli@yahoo.com>
2026-06-09 16:50:54 -0400

    remove .jac

--- littlex5/.jac/client/compiled/main.js
+++
@@ -1,8 +0,0 @@
-/* Source: /home/patrickli/Space/jaseci_env/jaseci/jac/examples/littleX/main.jac */
-import {__jacJsx, __jacSpawn} from "@jac/runtime";
-import { app as ClientApp } from "./frontend.js";
-function app() {
-  return __jacJsx(ClientApp, {}, []);
-}
-export {app};
-//# sourceMappingURL=main.js.map
