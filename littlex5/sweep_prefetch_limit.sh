8983935070389acbe86f127050b6cfd0a6f7a959
Patrick Li <baichuanli@yahoo.com>
2026-06-15 02:33:31 -0400

    sweep results: misc breakdown investigation (gc + redundant commits)

--- littlex5/sweep_prefetch_limit.sh
+++ littlex5/sweep_prefetch_limit.sh
@@ -3,7 +3,7 @@ set -e
 
 # Configuration
 # PREFETCH_LIMITS=(0 5000 10000 15000 20000 25000 30000)
-PREFETCH_LIMITS=(0 5000 10000 20000)
+PREFETCH_LIMITS=(0 10000)
 RESULTS_FILE="sweep_prefetch_limit.csv"
 
 # Clean all previous data
